51a6aed850f93e14767c73202649473731fb8f44
ReiiYuki <voraton.l@ku.th>
2017-06-17 09:47:10 +0900

    Gestural Listener

---
+++ Assets/Core/Scripts/GesturalListener.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public abstract class GesturalListener : MonoBehaviour{
+
+    public string gesture;
+
+    public abstract void Action();
+
+    void OnSetupSuccess()
+    {
+        GetComponent<GesturalManager>().AddListener(gesture, this);
+    }
+
+}
---
+++ Assets/Core/Scripts/GesturalManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GesturalManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddListener(string gesture, GesturalListener listener) {
+
+    }
+}
